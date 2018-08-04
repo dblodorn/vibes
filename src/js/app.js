37b7faed931983aff4743eb4906a8b92eaf7ac2c
Dain Blodorn Kim <dmbk@Dains-MacBook-Pro.local>
2018-08-03 17:44:47 -0700

    new stuff

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,23 @@
 import zoomSound from './scripts/zoomSound'
 
+const body = document.querySelector('body')
+
 const Loaded = () => {
-  zoomSound()
+  if (body.classList.contains('viber')) {
+    zoomSound({
+      time: 2000,
+      quant: 20,
+      rotate: 180,
+      pitch: 2
+    })
+  } else if (body.classList.contains('sliber')) {
+    zoomSound({
+      time: 8000,
+      quant: 40,
+      rotate: 360,
+      pitch: 4
+    })
+  }
 }
 
 window.onload = Loaded()
--- src/js/scripts/zoomSound.js
+++ src/js/scripts/zoomSound.js
@@ -2,7 +2,7 @@ import { tween, easing, styler } from 'popmotion'
 import { FMSynth, PingPongDelay } from  'tone'
 import quantizeNumber from 'quantize-number'
 
-export default () => {
+export default (config) => {
   const ball = document.querySelector('#ball')
   const ballStyler = styler(ball)
 
@@ -15,15 +15,15 @@ export default () => {
 
   const audioVis = (val) => {
     ballStyler.set(val)
-    fm.triggerAttackRelease(quantizeNumber((val.rotate * 2), 20), '6n');
+    fm.triggerAttackRelease(quantizeNumber((val.rotate * config.pitch), config.quant), '6n');
   }
 
   const updateCounter = (v) => audioVis(v);
 
   tween({
     from: 0,
-    to: { rotate: 180, scale: 20 },
-    duration: 5000,
+    to: { rotate: config.rotate, scale: 20 },
+    duration: config.time,
     ease: easing.linear,
     flip: Infinity
   }).start(updateCounter)
